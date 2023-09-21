a488609e8a88fd70fcbb9bd123f83dc94b0964b9
SsuLMG <seanseol@naver.com>
2023-09-21 12:01:31 +0900

    원점에서 원 운동 완료
    원점에서 원 운동 완료

--- character_grass.py
+++ character_grass.py
@@ -1,17 +1,23 @@
 from pico2d import *
+import math
 
 open_canvas()
 
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas_now()
-grass.draw_now(400,30)
-character.draw_now(400,90)
-delay(1)
+
 
 def run_circle():
     print('CIRCLE')
+    r = 200
+    for deg in range(0,360,5):
+        x = r * math.cos(math.radians(deg))
+        y = r * math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.1)
     pass
 
 def run_rectangle():
@@ -21,5 +27,6 @@ def run_rectangle():
 while True:
     run_rectangle()
     run_circle()
+    break
     
 close_canvas()
